c92897119abd644b3ad8360eacc1c9c29088d03c
gu24ia0015-alt <gu24ia0015@globaluniversity.edu.mx>
2026-08-05 20:31:40 -0600

    feat: add reusable CampoInput component with label and required indicator

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 ﻿import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Boton } from './shared/boton/boton';
+import { CampoInput } from './shared/input/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Boton],
+  imports: [RouterOutlet, Boton, CampoInput],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/shared/input/input.ts
@@ -0,0 +1,24 @@
+﻿import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './input.html',
+  styleUrl: './input.css'
+})
+export class CampoInput {
+  @Input() etiqueta: string = '';
+  @Input() tipo: string = 'text';
+  @Input() placeholder: string = '';
+  @Input() valor: string = '';
+  @Input() requerido: boolean = false;
+  @Output() valorCambio = new EventEmitter<string>();
+
+  onInput(event: Event) {
+    const nuevoValor = (event.target as HTMLInputElement).value;
+    this.valorCambio.emit(nuevoValor);
+  }
+}
